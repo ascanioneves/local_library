801f46d8d082fe15c25fa18372e1788f05bc0d0b
ascanioneves <ascaniosavio97@gmail.com>
2023-02-17 16:40:51 -0300

    feat: add catalog models (book and genre)

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+#Model representing a book genre
+class Genre(models.Model):
+    name = models.CharField(max_length=200, help_text='Enter a book genre (e.g. Science Fiction)')
+
+    #String for representing our model object
+    def __str__(self):
+        return self.name
+    
+class Book(models.Model):
+
+    title = models.CharField(max_length=200)
+
+    # Foreign key used because a book can only have one author, but author can have multiple books
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+
+    summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
+
+    isbn = models.CharField('ISBN', max_length=13, unique=True, 
+                            help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+    
+    # ManyToManyField used because genre can contain many books, and books can cover many genres.
+    # Genre class has already been defined so we can specify the object above.  
+    genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse("book_detail", args=[str(self.id)])
+    
+
+    
+
