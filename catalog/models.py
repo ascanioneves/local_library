0ab56fdd9e5a5deb57ff0da058234e1329fe8efe
ascanioneves <ascaniosavio97@gmail.com>
2023-02-17 18:17:17 -0300

    feat: add models (author, bookInstance)

--- catalog/models.py
+++ catalog/models.py
@@ -1,9 +1,12 @@
+import uuid
+
 from django.db import models
 from django.urls import reverse
 
 
 #Model representing a book genre
 class Genre(models.Model):
+
     name = models.CharField(max_length=200, help_text='Enter a book genre (e.g. Science Fiction)')
 
     #String for representing our model object
@@ -14,7 +17,7 @@ class Book(models.Model):
 
     title = models.CharField(max_length=200)
 
-    # Foreign key used because a book can only have one author, but author can have multiple books
+    # Foreign key used because a book can only have one author, but author can have multiple books (In this project)
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
 
     summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
@@ -33,5 +36,50 @@ class Book(models.Model):
         return reverse("book_detail", args=[str(self.id)])
     
 
+class BookInstance(models.Model):
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular book across whole library')
+
+    book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+
+    LOAN_STATUS = (
+        ('m', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+
+    status = models.CharField(
+        max_length=1,
+        choices=LOAN_STATUS,
+        blank=True,
+        default='m',
+        help_text='Book availability'
+    )
+    
+    class Meta:
+        ordering = ['due_back']
+
+    
+    def __str__(self):
+        return f'{self.id} ({self.book.title})'
     
 
+class Author(models.Model):
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def get_absolute_url(self):
+        return reverse("author_detail", args=[str(self.id)])
+    
+    def __str__(self):
+        return f'{self.last_name}, {self.first_name}'
+    
