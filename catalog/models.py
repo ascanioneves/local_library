f1715da111d4bf77b43cc7ff27bfd898ad655b2f
ascanioneves <ascaniosavio97@gmail.com>
2023-02-17 23:38:08 -0300

    feat: created language model and add It to Book table as a foreign key

--- catalog/models.py
+++ catalog/models.py
@@ -12,13 +12,22 @@ class Genre(models.Model):
     #String for representing our model object
     def __str__(self):
         return self.name
-    
+
+class Language(models.Model):
+
+    name = models.CharField(max_length=100, help_text="Enter the book's natural language (e.g. English, French, Japanese etc.)")
+
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
 
     title = models.CharField(max_length=200)
 
     # Foreign key used because a book can only have one author, but author can have multiple books (In this project)
+    #and the same to language
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
 
     summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
 
@@ -28,6 +37,7 @@ class Book(models.Model):
     # ManyToManyField used because genre can contain many books, and books can cover many genres.
     # Genre class has already been defined so we can specify the object above.  
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
+
 
     def __str__(self):
         return self.title
