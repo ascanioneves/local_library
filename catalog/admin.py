3c618d515e5c1606fcfced79f0dca11000f8799f
ascanioneves <ascaniosavio97@gmail.com>
2023-02-23 16:11:32 -0300

    feat: add  register decorator

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
+from .models import Author, Book, BookInstance, Genre, Language
+
 # Register your models here.
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
+
+
+#admin.site.register(Book)
+#admin.site.register(Author)
+admin.site.register(Genre)
+#admin.site.register(BookInstance)
+admin.site.register(Language)
